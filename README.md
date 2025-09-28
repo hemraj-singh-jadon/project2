0d247ed31e432fb48a1c8a74594016b618068fd2
Hemraj Singh Jadon <hemrajjadon4536@gmail.com>
2025-09-28 19:07:53 +0530

    changes made by hemraj

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This project was created from local system.
 created by Hemraj
+
+# Git and Github 
+learnig git and github
+
+# Branch
+learning something new about branchig
--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-// new feature added
+let h1 = document.querySelector("h1")
+let h3 = document.querySelector("h3")
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
 
-button = document.querySelector("button")
+btn1.addEventListener("click",()=> {
+    h1.style.color = 'magents'
+})
+
+btn2.addEventListener("click",()=> {
+    h3.style.color = 'pink'
+})
